dcf20fb4b17f28d323a30a6ada1a7854c5073be2
Matthew <105622101+m-wheeler-dev@users.noreply.github.com>
2023-11-09 17:18:45 -0800

    fix nav toggler

--- scripts.js
+++ scripts.js
@@ -6,7 +6,18 @@ const modeToggleBtn = document.querySelector("#mode-toggler");
 
 // TOGGLE NAV MENU
 const toggleMenu = () => {
+	const openNav = document.querySelector(".fa-bars-staggered");
+	const closeNav = document.querySelector(".fa-xmark");
+
 	nav.classList.toggle("sidebar");
+
+	if (nav.classList.contains("sidebar")) {
+		openNav.style.display = "none";
+		closeNav.style.display = "block";
+	} else {
+		openNav.style.display = "block";
+		closeNav.style.display = "none";
+	}
 };
 navToggler.addEventListener("click", toggleMenu);
 
